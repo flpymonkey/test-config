d576d7366429efd51ec1f92754502c7b05689156
Ben Johnson <benj@Bens-MacBook-Air.local>
2025-01-15 15:55:02 -0800

    Test API

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import './envConfig.ts'
 
 // Using this:
 // https://www.geeksforgeeks.org/middlewares-in-next-js/
@@ -6,14 +7,43 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function middleware(req: NextRequest): Promise<NextResponse> {
     // Get URL parameters
     const url = new URL(req.url);
-    const argA = url.searchParams.get('argA');
-    const argB = url.searchParams.get('argB');
+    const tagId = url.searchParams.get('tagId');
+    const eCode = url.searchParams.get('eCode');
+    const enc = url.searchParams.get('e');
+    const cmac = url.searchParams.get('c');
 
-    if (argA === "gfg" && argB === "123") {
-        return NextResponse.redirect(new URL('/success', req.url));    
+    if (tagId && eCode && enc && cmac) {
+        // Perform the API call
+        const res = await fetch(
+            'https://third-party.etrnl.app/v1/tags/verify-authenticity',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'API-KEY': process.env.ETRNL_KEY!
+                },
+                body: JSON.stringify({ tagId, eCode, enc, cmac })
+            }
+        );
+
+        const { success, exists, authentic, ctr, uid, err } = await res.json();
+
+        // Handle the response from the API call
+        if (success) {
+            return NextResponse.redirect(new URL('/success', req.url));
+        } else {
+            return new NextResponse(
+                `<h1>ETRNL API Call Failed</h1><p>Error: ${err}</p>`,
+                {
+                    status: 401,
+                    headers: { 'Content-Type': 'text/html' }
+                }
+            );
+        }
     } else {
+        const params = Array.from(url.searchParams.entries()).map(([key, value]) => `${key}: ${value}`).join(', ');
         return new NextResponse(
-            `<h1>Invalid argA & argB</h1>`,
+            `<h1>Missing required arguments</h1><p>Received arguments: ${params}</p>`,
             {
                 status: 401,
                 headers: { 'Content-Type': 'text/html' }
