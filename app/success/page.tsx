a2c63c15731a276c07a25db44f6cded3260823a3
Ben Johnson <benj@Bens-MacBook-Air.local>
2025-01-15 23:10:41 -0800

    Test

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import React from 'react';
-// import MuxPlayer from "@mux/mux-player-react"; 
-import ReactPlayer from 'react-player/lazy';
-
+import MuxPlayer from "@mux/mux-player-react"; 
 import { useCookies } from 'next-client-cookies';
 
 const VideoPlayer: React.FC = () => {
@@ -16,12 +14,11 @@ const VideoPlayer: React.FC = () => {
   }
 
   return (
-    // <MuxPlayer
-    //   streamType="on-demand"
-    //   playbackId={playbackId}
-    //   tokens={{ playback: muxToken }}
-    // />
-    <ReactPlayer url="https://stream.mux.com/a4nOgmxGWg6gULfcBbAa00gXyfcwPnAFldF8RdsNyk8M.m3u8" controls={true} width="100%" height="100%" />
+    <MuxPlayer
+      streamType="on-demand"
+      playbackId={playbackId}
+      tokens={{ playback: muxToken }}
+    />
   );
 };
 
@@ -31,15 +28,16 @@ const SuccessPage: React.FC = () => {
       <h1>Success!</h1>
       <p>You have successfully passed ETRNL Authentication.</p>
 
-      {/* <MuxPlayer
-        playbackId="a4nOgmxGWg6gULfcBbAa00gXyfcwPnAFldF8RdsNyk8M"
-        metadata={{
-          video_id: "video-id-54321",
-          video_title: "Test video title",
-          viewer_user_id: "user-id-007",
-        }}
-      /> */}
-      <ReactPlayer url="https://stream.mux.com/a4nOgmxGWg6gULfcBbAa00gXyfcwPnAFldF8RdsNyk8M.m3u8" controls={true} width="100%" height="100%" />
+      <VideoPlayer />
+
+      <MuxPlayer
+      playbackId="a4nOgmxGWg6gULfcBbAa00gXyfcwPnAFldF8RdsNyk8M"
+      metadata={{
+        video_id: "video-id-54321",
+        video_title: "Test video title",
+        viewer_user_id: "user-id-007",
+      }}
+    />
     </div>
   );
 };
