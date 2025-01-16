fec1038a5cd0a905b3ae0092acd17a7f817851e5
Ben Johnson <benj@Bens-MacBook-Air.local>
2025-01-15 16:18:38 -0800

    Updated ETRNL url demo

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -4,7 +4,7 @@ const SuccessPage: React.FC = () => {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Success!</h1>
-      <p>You have successfully passed the middleware check.</p>
+      <p>You have successfully passed ETRNL Authentication.</p>
     </div>
   );
 }
